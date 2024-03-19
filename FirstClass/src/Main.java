c6e608ec30a8ab8fde4ee509549a318baf381557
MochFikryDeniansyah <116334041+MochFikryDeniansyah@users.noreply.github.com>
2024-03-19 12:59:13 +0700

    Add files via upload

---
+++ FirstClass/src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello My Name Moch Fikry Deniansyah");
+        //
+        System.out.println("My Age 22");
+        System.out.println("Ini adalah branch development");
+
+        boolean isAlien = false;
+
+        if (isAlien == false) { /*!isAlien*/
+            System.out.println("im scared of alien");
+            System.out.println("its not an alien!!");
+        }
+
+
+        System.out.println("Who i a'm?");
+
+    }
+}
